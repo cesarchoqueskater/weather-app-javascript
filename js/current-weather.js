531f3ae203a0964e210b6fd443105dc848d771ed
cchoqueh <cesarchoqueskater@gmail.com>
2021-09-21 16:52:34 -0500

    Renderizando la ciudad de Origen

--- js/current-weather.js
+++ js/current-weather.js
@@ -1,15 +1,24 @@
 import weather from '../data/current-weather.js'
 
+function setCurrentCity($el, city) {
+    $el.textContent = city
+}
+
 function configCurrentWeather(weather) {
 
     // loader
     // date
     // city
+    const $currentWeatherCity = document.querySelector('#current-weather-city')
+    const city = weather.name
+    setCurrentCity($currentWeatherCity, city)
+    debugger
     // temp
     // background
 }
 
 export default function currentWeather() {
+    //GEO // API - weather //Config
     configCurrentWeather(weather)
     console.log(weather)
 }
