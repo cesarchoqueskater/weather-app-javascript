20c117176da1e2b83083f3c916d040fc31314cf8
cchoqueh <cesarchoqueskater@gmail.com>
2021-09-21 16:26:23 -0500

    Weather App, 1er commit

---
+++ js/current-weather.js
@@ -0,0 +1,15 @@
+import weather from '../data/current-weather.js'
+
+function configCurrentWeather(weather) {
+
+    // loader
+    // date
+    // city
+    // temp
+    // background
+}
+
+export default function currentWeather() {
+    configCurrentWeather(weather)
+    console.log(weather)
+}
